a2d7671c6d436dce87333537bad6a02023d2e547
Ben <benhv@stud.ntnu.no>
2023-12-24 15:07:28 +0100

    random commit.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,15 @@
+<script>
+  import Typewriter, { concurrent } from 'svelte-typewriter'
+</script>
+
 <svelte:head>
   <title> HOME </title>
 </svelte:head>
 
+<section class="flex flex-row flex-wrap items-center justify-center min-h-screen">
+  <div class="w-full text-center">
+    <Typewriter interval={80}>
+      <h1 class="font-serif text-3xl mb-2">👋VELKOMMEN TE EBS 32</h1>
+    </Typewriter>
+  </div>
+</section>
