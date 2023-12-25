49703f677b2f4178fbc293c9e2d7bea18d564a83
Ben <benhv@stud.ntnu.no>
2023-12-26 00:24:51 +0100

    remove clear text functionality.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,6 @@
 <script>
   import Typewriter from 'svelte-typewriter';
-  import { onMount } from 'svelte';
-
-  let text = "👋VELKOMMEN TIL EBS 32";
-  let clearAfter = 1000;
-
-  let showText = true;
-
-  onMount(() => {
-    let typingDuration = text.length * 100
-
-    setTimeout(() => {
-      showText = false;
-    }, typingDuration + clearAfter)
-
-  })
-
+  let text = "👋VELKOMMEN TIL EBS 32"; 
 </script>
 
 <svelte:head>
@@ -24,10 +9,8 @@
 
 <section class="flex flex-colflex-wrap items-center justify-center">
   <div class="w-full text-center">
-    {#if showText}
       <Typewriter interval={80}>
         <h1 class="font-serif text-3xl mb-2">{text}</h1>
       </Typewriter>
-    {/if}
     </div> 
 </section>
