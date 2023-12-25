c3e6e7aaec0930ec0edcfa9c641d295c03dbeae4
Ben <benhv@stud.ntnu.no>
2023-12-25 21:55:10 +0100

    change from <a> to <Link>

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -14,32 +14,25 @@
         padding: 10px 20px;
         box-shadow: 0 2px 4px rgba(0,0,0,0.1);
     }
-
-    .title {
-        color: #b41f1f;
-        text-align: center;
-        margin-top: 20px;
-        font-size: 28px;
-        font-weight: bold;
-    }
-
-  :global(.menu .style-link) {
-    text-decoration: none;
-    color: #333;
-    padding: 10px 20px;
-    font-family: 'Arial', sans-serif;
+    
+    :global(.menu .style-link) {
+        text-decoration: none;
+        color: #333;
+        padding: 10px 20px;
+        font-family: 'Arial', sans-serif;
     }       
 </style>
 
 <Router>
     <div class="customheader">
-        <a href="/" class="title font-serif no-underline text-black">EBS 32</a>
+        <Link to="/" class="title font-serif no-underline text-black">EBS 32</Link>
         <nav class="menu">
             <Link to="/about" class="style-link font-serif">Om steinan</Link>
             <Link to="/members" class="style-link font-serif">Medlemmer</Link>
         </nav>
     </div>
         <main>
+            <Route path="/" component={Page} />
             <Route path="about" component={About}/>
             <Route path="members" component={Members}/>
         </main>
